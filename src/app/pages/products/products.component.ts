3dbda905b5fc6517ff17de4e073efcc4a0a9ac4c
byNethZ <bynethz@gmail.com>
2022-04-18 23:38:32 -0500

    Services created, call to api

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { Product } from './interface/product.interface';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  products : Product[] = [];
 
-  constructor() { }
+  constructor(
+    private _productService : ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this._productService.getProducts()
+    .pipe(
+      tap( (products : Product[]) => this.products = products )
+    )
+    .subscribe();
   }
 
 }
---
+++ src/app/pages/products/services/products.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../interface/product.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private apiUrl = 'http://localhost:3000/products';
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getProducts() : Observable<Product[]>{
+    return this._http.get<Product[]>(this.apiUrl);
+  }
+}
